2dbe55318b00f81ecbc7d60ab6555ea1dd633482
Young2un <popduddms@gmail.com>
2025-02-26 03:43:22 +0900

    feat: 메인페이지 1차 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import SearchBar from "./_components/SearchBar";
+
 // 메인페이지
 export default async function Home() {
   const response = await fetch("http://localhost:3000/api/books");
   const data = await response.json();
   console.log(data);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      home
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="w-6/12">
+        <SearchBar />
+        <Link href="/books">
+          <Button className="w-full text-lg py-7">📚 전체 책 목록 보기</Button>
+        </Link>
+      </div>
     </main>
   );
 }
