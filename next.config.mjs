13401c0ab42918e87a7014478cf81b980f7f6dcc
Young2un <popduddms@gmail.com>
2025-02-26 23:42:00 +0900

    fix: 이미지 경로 허용 config 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "shopping-phinf.pstatic.net", // ✅ 허용할 이미지 도메인 추가
+      },
+    ],
+  },
+};
 export default nextConfig;
