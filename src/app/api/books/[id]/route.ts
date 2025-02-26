57a016ceb55ef9506c84f36e739f1e1d81224230
Young2un <popduddms@gmail.com>
2025-02-27 03:16:57 +0900

    feat: 상세페이지 GET 구현

--- src/app/api/books/[id]/route.ts
+++ src/app/api/books/[id]/route.ts
@@ -1,4 +1,23 @@
-// 기본세팅
-export async function GET() {}
+import { createClient } from "@/utils/supabase/server";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("books")
+    .select("*")
+    .eq("id", params.id)
+    .single();
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json(data, { status: 200 });
+}
+
 export async function PUT() {}
 export async function DELETE() {}
