4a4d46fb2b4592a31b5c5020fac535caeaca1d5b
Young2un <popduddms@gmail.com>
2025-02-26 23:37:28 +0900

    feat: 검색어 미입력시 경고문구 추가

--- src/app/_components/SearchBar.tsx
+++ src/app/_components/SearchBar.tsx
@@ -3,34 +3,57 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
-export default function SearchBar() {
-  const [searchQuery, setSearchQuery] = useState("");
+interface SearchBarProps {
+  search: string;
+}
+
+export default function SearchBar({ search }: SearchBarProps) {
+  const [searchQuery, setSearchQuery] = useState(search);
+  const [error, setError] = useState("");
   const router = useRouter();
 
   const handleSearch = (e: React.FormEvent) => {
     e.preventDefault();
+    if (!searchQuery.trim()) {
+      setError("검색어를 입력해주세요.");
+      return;
+    }
+    setError("");
     if (searchQuery.trim()) {
       router.push(`/books?search=${searchQuery}`);
     }
   };
 
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+    if (error) {
+      setError("");
+    }
+  };
+
   return (
-    <form
-      onSubmit={handleSearch}
-      className="w-full flex gap-2 mb-6 items-center "
-    >
-      <Input
-        type="text"
-        placeholder="책 제목 또는 저자로 검색하기"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        className="w-full py-7 text-lg placeholder:text-lg"
-      />
-      <Button type="submit" className="py-7 w-40 text-lg">
-        검색
-      </Button>
+    <form onSubmit={handleSearch} className="w-full flex flex-col gap-2 mb-2">
+      <div className="flex gap-2 items-center">
+        <Input
+          type="text"
+          placeholder="책 제목 또는 저자로 검색하기"
+          value={searchQuery}
+          onChange={handleOnChange}
+          className="w-full py-7 text-lg  placeholder:text-lg text-inherit"
+        />
+        <Button type="submit" className="py-7 w-40 text-lg">
+          검색
+        </Button>
+      </div>
+      <p
+        className={`text-red-500 text-sm h-5 transition-opacity duration-300 ${
+          error ? "opacity-100 visible" : "opacity-0 invisible"
+        }`}
+      >
+        {error}
+      </p>
     </form>
   );
 }
