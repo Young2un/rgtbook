4bba9fc10c50791068a18b21b28e9613a0cf9231
Young2un <popduddms@gmail.com>
2025-02-26 23:41:25 +0900

    feat: 책필터링 및 페이지네이션 구현

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,14 +1,36 @@
 import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 
-// 기본세팅
-export async function GET() {
+export async function GET(req: Request) {
   const supabase = createClient();
-  const { data, error } = await supabase.from("books").select();
+  const { search, page } = Object.fromEntries(new URL(req.url).searchParams);
 
-  if (error)
+  const PAGE_SIZE = 10;
+  const pageNumber = parseInt(page) || 1;
+  const start = (pageNumber - 1) * PAGE_SIZE;
+  const end = start + PAGE_SIZE - 1;
+
+  let query = supabase
+    .from("books")
+    .select("*", { count: "exact" })
+    .range(start, end);
+
+  if (search) {
+    query = query.or(`title.ilike.%${search}%,author.ilike.%${search}%`);
+  }
+
+  const { data: books, count, error } = await query;
+
+  if (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 
-  return NextResponse.json(data, { status: 200 });
+  const totalPages = Math.ceil((count ?? 0) / PAGE_SIZE);
+
+  return NextResponse.json(
+    { books, total: count, totalPages },
+    { status: 200 }
+  );
 }
+
 export async function POST() {}
